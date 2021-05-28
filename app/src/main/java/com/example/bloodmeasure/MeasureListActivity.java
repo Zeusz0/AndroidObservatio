830d455374e61270c19329db08dbf51217c9ffd6
Zeusz0 <2000zeno@gmail.com>
2021-05-28 21:02:18 +0200

    5. anyag kész

---
+++ app/src/main/java/com/example/bloodmeasure/MeasureListActivity.java
@@ -0,0 +1,31 @@
+package com.example.bloodmeasure;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MeasureListActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MeasureListActivity.class.getName();
+    private FirebaseUser user;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_measure_list);
+        mAuth = FirebaseAuth.getInstance();
+        // mAuth.signOut();
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if(user != null) {
+            Log.d(LOG_TAG, "Authenticated user!");
+        } else {
+            Log.d(LOG_TAG, "Unauthenticated user!");
+            finish();
+        }
+    }
+}
